109a085c0e8a8e3b2699f7e230e4a10ab6834cc2
Beifei Ning (IT) <Beifei.Ning@cicc.com.cn>
2023-03-08 17:53:54 +0800

    feat(core): spin lock

---
+++ core/spinlock.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file	spinlock.hpp
+ * @brief 
+ * @author	ningbf
+ * @version 1.0.0
+ * @date	2023-03-08
+ */
+#ifndef _SPIN_LOCK_HPP_
+#define _SPIN_LOCK_HPP_ 
+
+#include <atomic> 
+class spinlock{
+	public:
+		spinlock():flag_(ATOMIC_FLAG_INIT){
+
+		}
+		~spinlock(){}
+		
+		void lock(){
+			while(flag_.test_and_set(std::memory_order_acquire));	
+		}
+		void unlock(){
+			flag_.clear(std::memory_order_release);
+		}
+	private:
+		std::atomic_flag			flag_;
+};
+
+
+#endif 
+
