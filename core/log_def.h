40f54d0b7da975c51e3225d0db6de8536daf7ffe
Beifei Ning (IT) <Beifei.Ning@cicc.com.cn>
2023-03-09 18:11:06 +0800

    fix(log): sys_log

--- core/log_def.h
+++ core/log_def.h
@@ -21,7 +21,7 @@
 #define  get_file_name(filename)  ((filename == NULL || strrchr(filename, '/') == NULL)?"":strrchr(filename, '/') + 1)
 #define trace_print(fmt, ...)			do{ struct timeval tv; gettimeofday(&tv, NULL); struct tm tmNow; localtime_r(&tv.tv_sec, &tmNow);  printf("%04d-%02d-%02d %02d:%02d:%02d.%06ld [TRACE] " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.tv_usec, ##__VA_ARGS__);} while (0)
 #define info_print(fmt, ...)			do{ struct timeval tv; gettimeofday(&tv, NULL); struct tm tmNow; localtime_r(&tv.tv_sec, &tmNow);  printf("%04d-%02d-%02d %02d:%02d:%02d.%06ld [INFO] " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.tv_usec, ##__VA_ARGS__);} while (0)
-#define error_print(fmt, ...)			do { struct timeval tv; gettimeofday(&tv, NULL); struct tm tmNow; localtime_r(&tv.tv_sec, &tmNow);  printf("%04d-%02d-%02d %02d:%02d:%02d.%06ld [ERROR] " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.tv_usec, get_file_name(__FILE__), __LINE__, ##__VA_ARGS__);} while (0)
+#define error_print(fmt, ...)			do { struct timeval tv; gettimeofday(&tv, NULL); struct tm tmNow; localtime_r(&tv.tv_sec, &tmNow);  printf("%04d-%02d-%02d %02d:%02d:%02d.%06ld [ERROR] %s(%d)" fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.tv_usec, get_file_name(__FILE__), __LINE__, ##__VA_ARGS__);} while (0)
 #else 
 #include <varargs.h> 
 #include <sys/timeb.h>
