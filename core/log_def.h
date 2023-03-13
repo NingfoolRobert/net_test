3e4a9b30127ebd679d121462ba74025510a465c0
Beifei.Ning <Beifei.Ning@cicc.com.cn>
2023-03-13 10:46:55 +0800

    fix(core): system log

--- core/log_def.h
+++ core/log_def.h
@@ -26,9 +26,9 @@
 #include <varargs.h> 
 #include <sys/timeb.h>
 #define  get_file_name(filename)  ((filename == NULL || strrchr(filename, '\\') == NULL)? "": strrchr(filename, '\\') + 1)
-#define trace_print(fmt, ...)			do{ timeb tv; ftime(&tv); struct tm tmNow; localtime_s(&tmNow, &tv.time);  printf("%04d-%02d-%02d %02d:%02d:%02d.%03d [TRACE] " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tv.millitm, ##__VA_ARGS__); } while (0)
-#define info_print(fmt, ...)			do{ timeb tv; ftime(&tv); struct tm tmNow; localtime_s(&tmNow, &tv.time);  printf("%04d-%02d-%02d %02d:%02d:%02d.%03d [INFO] " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tv.millitm, ##__VA_ARGS__); } while (0)
-#define error_print(fmt, ...)			do { timeb tv; ftime(&tv); struct tm tmNow; localtime_s(&tmNow, &tv.time);  printf("%04d-%02d-%02d %02d:%02d:%02d.%03d [ERROR] %s(%d)" fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.millitm, get_file_name(__FILE__), __LINE__,##__VA_ARGS__);} while (0)
+#define trace_print(fmt, ...)			do{ timeb tv; ftime(&tv); struct tm tmNow; localtime_s(&tmNow, &tv.time);  printf("%04d-%02d-%02d %02d:%02d:%02d.%03d [TRACE] " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.millitm, ##__VA_ARGS__); } while (0)
+#define info_print(fmt, ...)			do{ timeb tv; ftime(&tv); struct tm tmNow; localtime_s(&tmNow, &tv.time);  printf("%04d-%02d-%02d %02d:%02d:%02d.%03d [INFO] " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.millitm, ##__VA_ARGS__); } while (0)
+#define error_print(fmt, ...)			do{ timeb tv; ftime(&tv); struct tm tmNow; localtime_s(&tmNow, &tv.time);  printf("%04d-%02d-%02d %02d:%02d:%02d.%03d [ERROR] %s(%d) " fmt, tmNow.tm_year + 1900,tmNow.tm_mon + 1, tmNow.tm_mday, tmNow.tm_hour, tmNow.tm_min, tmNow.tm_sec, tv.millitm, get_file_name(__FILE__), __LINE__,##__VA_ARGS__);} while (0)
 #endif 
 #else 
 #define 	trace_print(fmt, ...)		
