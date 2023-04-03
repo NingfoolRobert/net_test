d1b0a23ca2f5da87ad7789fd3e4f8566c8c421e4
Beifei.Ning <Beifei.Ning@cicc.com.cn>
2023-04-03 09:09:55 +0800

    perf(core): simple token

--- core/simple_token.cpp
+++ core/simple_token.cpp
@@ -17,11 +17,6 @@ void simple_token::wait()
 {
 	while (true) {
 		auto now = std::chrono::steady_clock::now().time_since_epoch().count();
-		if (_time_point == 0) {
-			_time_point = now;
-			return;
-		}
-
 		if (_time_point + _rate <= now) {
 			_time_point = now;
 			return;
