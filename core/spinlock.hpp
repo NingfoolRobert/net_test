2bf02ac977b5e55ff4863e760b2d27807995ee5a
NingfoolRobert <371494724@qq.com>
2024-12-23 12:53:25 +0800

    [perf]: spinlock

--- core/spinlock.hpp
+++ core/spinlock.hpp
@@ -1,38 +1,59 @@
 /**
  * @file	spinlock.hpp
- * @brief 
+ * @brief
  * @author	ningbf
  * @version 1.0.0
  * @date	2023-03-08
  */
 #ifndef _SPIN_LOCK_HPP_
-#define _SPIN_LOCK_HPP_ 
+#define _SPIN_LOCK_HPP_
 
-#include <atomic> 
+#include <atomic>
 #include <exception>
-class spinlock{
-	public:
-		spinlock(){
-		}
-		~spinlock(){}
-		//
-		spinlock(const spinlock&)				= delete;
-		spinlock& operator=(const spinlock&)	= delete;	
-		//
-		void lock(){
-#ifndef _NULLLOCK_
-			while(flag_.test_and_set(std::memory_order_acquire));	
-#endif //_NULLLOCK_ 
-		}
-		void unlock(){
-#ifndef _NULLLOCK_
-			flag_.clear(std::memory_order_release);
-#endif //_NULLLOCK_
-		}
-	private:
-		std::atomic_flag			flag_ = ATOMIC_FLAG_INIT;
+#include <sched.h>
+
+class nulllock {
+    nulllock() = default;
+    ~nulllock() = default;
+    nulllock(const nulllock &) = delete;
+    nulllock &operator=(const nulllock &) = delete;
+
+    void lock() {
+    }
+    bool try_lock() {
+        return true;
+    }
+    void unlock() {
+    }
 };
 
+class spinlock {
+public:
+    spinlock() {
+    }
+    ~spinlock() {
+    }
+    //
+    spinlock(const spinlock &) = delete;
+    spinlock &operator=(const spinlock &) = delete;
+    //
+    void lock() {
+        while (flag_.test_and_set(std::memory_order_acquire))
+        {
+            sched_yield();
+        }
+    }
+    //
+    bool try_lock() {
+        return !flag_.test_and_set(std::memory_order_acquire);
+    }
+    //
+    void unlock() {
+        flag_.clear(std::memory_order_release);
+    }
 
-#endif 
+private:
+    std::atomic_flag flag_ = ATOMIC_FLAG_INIT;
+};
 
+#endif
