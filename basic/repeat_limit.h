7c7a0492049779827a534a111547e07d3ea71a66
NingfoolRobert <371494724@qq.com>
2025-12-25 21:47:14 +0800

    feat: add repeat filter limit

--- basic/repeat_limit.h
+++ basic/repeat_limit.h
@@ -11,12 +11,19 @@
 
 namespace basic {
 
+/** @brief A class to control repetition behavior based on thresholds and bars.
+ * This class helps in determining whether an operation should be repeated
+ * based on the number of times it has been invoked.
+ */
 class RepeatLimit {
 public:
-    RepeatLimit(size_t bar1, size_t threshold1, size_t bar2, size_t threshold2)
+    RepeatLimit(size_t bar1, size_t threshold1, size_t bar2 = 50 * 1000, size_t threshold2 = 100 * 1000)
         : counter_(0), bar1_(bar1), bar2_(bar2), threshold1_(threshold1), threshold2_(threshold2) {
     }
-
+    /**
+     * @brief Determines whether to repeat based on the configured limits.
+     * @return true if the operation should be repeated; false otherwise.
+     */
     bool repeat() {
         size_t counter = ++counter_;
         if (counter < bar1_) {
@@ -35,6 +42,13 @@ public:
         }
 
         return false;
+    }
+    /**
+     * @brief Retrieves the current count of repetitions.
+     * @return The current repetition count.
+     */
+    size_t counter() const {
+        return counter_;
     }
 
 private:
